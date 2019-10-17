31db1ecb454830db6c174d1a785f1b4fc600bf76
Aditya Kalra <51115390+adityakalra581@users.noreply.github.com>
2019-10-17 18:42:52 +0530

    Swapcase
    Swapping lowercase and uppercase characters of a string.

---
+++ Swapcase.py
@@ -0,0 +1,12 @@
+
+# SWAPCASE:
+# Swapping Uppercase and Lowercase characters.
+
+def swap_case(s):
+    c = s.swapcase() 
+    return c
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
